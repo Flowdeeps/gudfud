d531ca5782e811e25992320ea44bd42ab2956d94
Flowdeeps <speedwolf@gmail.com>
2016-05-18 21:59:34 +0100

    OK, we're getting the title string now

--- python/parse.py
+++ python/parse.py
@@ -4,7 +4,7 @@ Best run from the root and using "input" when prompted
 '''
 
 import os
-import BeautifulSoup
+from bs4 import BeautifulSoup
 
 print "Enter a path relative to your current location:"
 userInput = raw_input()
@@ -13,8 +13,17 @@ print "userInput was " + path
 
 L = [f for f in os.listdir(path) if not f.startswith('.')]
 
+def doHTML(L):
+    html = open(os.path.join(path, fileName),'r').read()
+    soup = BeautifulSoup(html, "html5lib")
+    title = soup.title.string.replace("BBC Food - Recipes - ","")
+    print title ''' this is the title string '''
+
 if not L.__len__() == 0:
     for index, fileName in enumerate(L):
-        print index, fileName
+        '''
+        do the thing
+        '''
+        doHTML(L)
 else:
     print "No files found"
