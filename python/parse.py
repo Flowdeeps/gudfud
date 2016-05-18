4ed32ca8bb145001519bfc7476f3fe1cd351744c
Flowdeeps <speedwolf@gmail.com>
2016-05-18 18:48:04 +0100

    Starting to parse the files

--- python/parse.py
+++ python/parse.py
@@ -1,3 +1,14 @@
 import os
-import beautifulsoup
-import pymongo
+import BeautifulSoup
+
+print "Enter a path relative to your current location:"
+userInput = raw_input()
+path = os.path.abspath(userInput)
+print "userInput was " + path
+
+L = [f for f in os.listdir(path) if not f.startswith('.')]
+
+if not L.__len__() == 0:
+    print L
+else:
+    print "No files found"
