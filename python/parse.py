3a8db7cbafe9608e21331915602bb1f39c743780
Flowdeeps <speedwolf@gmail.com>
2016-05-19 23:34:42 +0100

    added support for utf-8 encoding

--- python/parse.py
+++ python/parse.py
@@ -4,24 +4,29 @@ Best run from the root and using "input" when prompted
 '''
 
 import os
+import codecs
 from bs4 import BeautifulSoup
-
 print "Enter a path relative to your current location:"
+'''
 userInput = raw_input()
+'''
+'''
 path = os.path.abspath(userInput)
+'''
+path = "input"
 print "userInput was " + path
 
 L = [f for f in os.listdir(path) if not f.startswith('.')]
 
 def doHTML(L):
-    html = open(os.path.join(path, fileName),'r').read()
+    html = codecs.open(os.path.join(path, fileName),'r','utf-8').read()
     soup = BeautifulSoup(html, "html5lib")
     ''' title property '''
     title = soup.title.string.replace("BBC Food - Recipes - ","")
     print title
-    matchTag = ["p", "a"]
+    matchTag = ["p", "a", "li"]
     matchAttr = ["class", "itemprop"]
-    matchClass = ["recipe-metadata__prep-time", "recipe-metadata__cook-time", "recipe-metadata__serving", "recipe-metadata__dietary-vegetarian-text", "recipe-description__text", "recipe-metadata__recommendations", "author"]
+    matchClass = ["recipe-metadata__prep-time", "recipe-metadata__cook-time", "recipe-metadata__serving", "recipe-metadata__dietary-vegetarian-text", "recipe-description__text", "recipe-metadata__recommendations", "author", "recipe-ingredients__list-item"]
     for j in range(len(matchTag)):
         for n in range(len(matchAttr)):
             for i in range(len(matchClass)):
@@ -49,6 +54,9 @@ def doHTML(L):
                         if not author.img:
                             author = elem.text
                             print "author: " + author
+                    elif ("recipe-ingredients" in matchClass[i]):
+                        ingredient = elem.text
+                        print ingredient
 if not L.__len__() == 0:
     for index, fileName in enumerate(L):
         '''
